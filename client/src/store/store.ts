20ffbac23af5705d697551b8d68411015f614ea7
Prashant Adhikari <student@prashantadhikari7.com.np>
2026-07-10 21:05:52 +0545

    chore(client): update redux store configuration

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,11 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authApi } from './api/authApi';
+import { visitingPlaceApi } from './api/visitingPlaceApi';
+import { visitingRoutesApi } from './api/visitingRoutesApi';
+import { userProgressApi } from './api/userProgressApi';
+import { userApi } from './api/userApi';
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+    [visitingPlaceApi.reducerPath]: visitingPlaceApi.reducer,
+    [visitingRoutesApi.reducerPath]: visitingRoutesApi.reducer,
+    [userProgressApi.reducerPath]: userProgressApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      authApi.middleware,
+      visitingPlaceApi.middleware,
+      visitingRoutesApi.middleware,
+      userProgressApi.middleware,
+      userApi.middleware,
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
